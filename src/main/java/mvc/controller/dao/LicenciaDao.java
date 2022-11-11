a121dcdffb7313dc1a8a1ee1d9caf51861a4b208
JuaniVega <juanivega0206@gmail.com>
2022-11-11 18:45:35 -0300

    Crea gestores y daos de Licencia y Persona

--- src/main/java/mvc/controller/dao/LicenciaDao.java
+++ src/main/java/mvc/controller/dao/LicenciaDao.java
@@ -1,5 +1,24 @@
 package mvc.controller.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import mvc.model.TipoLicencia;
+import util.ConexionP;
+
 public class LicenciaDao {
+
+	public static List<TipoLicencia> getTipoLicencia() throws Exception {
+		try {
+			String query = "select tl.id from public.tipo_licencia tl ;";                            
+			ArrayList<TipoLicencia> tipoLicencia = (ArrayList<TipoLicencia>)((Object) ConexionP.consultar(query, TipoLicencia.class));
+			return tipoLicencia;
+		}
+		catch(Exception ex) {
+			throw ex;
+		}
+	}
+	
+	
 
 }
