525bbeb211117935734e0e399b71a9bdcca82957
Matias Ifran <ifran2855@gmail.com>
2022-11-11 17:33:49 -0300

    creo los model

--- src/main/java/mvc/model/Persona.java
+++ src/main/java/mvc/model/Persona.java
@@ -1,5 +1,46 @@
 package mvc.model;
 
-public class Persona {
+import java.sql.Date;
 
+public class Persona {
+	
+	private String nombre;
+    private String apellido;
+    private String dni;
+    private TipoDocumento tipo_doc;
+    private Date fecha_nacimiento;
+    
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public TipoDocumento getTipo_doc() {
+		return tipo_doc;
+	}
+	public void setTipo_doc(TipoDocumento tipo_doc) {
+		this.tipo_doc = tipo_doc;
+	}
+	public Date getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+	public void setFecha_nacimiento(Date fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+    
+    
+    
 }
--- src/main/java/mvc/model/TipoGrupoSanguineo.java
+++ src/main/java/mvc/model/TipoGrupoSanguineo.java
@@ -2,4 +2,15 @@ package mvc.model;
 
 public class TipoGrupoSanguineo {
 
+	private String tipo_grupo_sanguineo;
+
+	public String getTipo_grupo_sanguineo() {
+		return tipo_grupo_sanguineo;
+	}
+
+	public void setTipo_grupo_sanguineo(String tipo_grupo_sanguineo) {
+		this.tipo_grupo_sanguineo = tipo_grupo_sanguineo;
+	}
+	
+	
 }
