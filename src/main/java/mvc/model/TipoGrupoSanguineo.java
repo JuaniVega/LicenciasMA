e8b22d1750aedb8446a4389fa6671a23925990b8
Matias Ifran <ifran2855@gmail.com>
2022-11-12 12:29:37 -0300

    model

--- src/main/java/mvc/model/TipoGrupoSanguineo.java
+++ src/main/java/mvc/model/TipoGrupoSanguineo.java
@@ -27,15 +27,4 @@ public class TipoGrupoSanguineo {
 		}
 	}
 
-	private String tipo_grupo_sanguineo;
-
-	public String getTipo_grupo_sanguineo() {
-		return tipo_grupo_sanguineo;
-	}
-
-	public void setTipo_grupo_sanguineo(String tipo_grupo_sanguineo) {
-		this.tipo_grupo_sanguineo = tipo_grupo_sanguineo;
-	}
-	
-	
 }
