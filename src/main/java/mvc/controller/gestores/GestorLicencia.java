d504a5b99c6137983df80f8c0e6b0782b4489e4e
Matias Ifran <ifran2855@gmail.com>
2022-11-12 17:55:12 -0300

    gestor All licencias

--- src/main/java/mvc/controller/gestores/GestorLicencia.java
+++ src/main/java/mvc/controller/gestores/GestorLicencia.java
@@ -3,12 +3,17 @@ package mvc.controller.gestores;
 import java.util.List;
 
 import mvc.controller.dao.LicenciaDao;
+import mvc.model.Licencia;
 import mvc.model.TipoLicencia;
 
 public class GestorLicencia {
 	
 	public static List<TipoLicencia> obtenerTipoLicencia() throws Exception{
 		return LicenciaDao.getTipoLicencia();
+	}
+
+	public static List<Licencia> obtenerAllLicencias() throws Exception{
+		return LicenciaDao.getAllLicencia();
 	}
 
 }
