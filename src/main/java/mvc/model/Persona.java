8f78f0a044485ac844d7fc867e5ac31fbc91ee03
Matias Ifran <ifran2855@gmail.com>
2022-11-15 12:44:27 -0300

    modifico persona model

--- src/main/java/mvc/model/Persona.java
+++ src/main/java/mvc/model/Persona.java
@@ -12,33 +12,20 @@ public class Persona {
 	protected LocalDate fechaNacimiento;
 	protected String sexo;
 	
-	public Persona(Integer idPersona, Integer dni, Integer tipoDoc, String nombre, String apellido, LocalDate fechaNacimiento, String sexo) {
-		super();
-		this.idPersona = idPersona;
-		this.dni=dni;
-		this.tipoDoc = tipoDoc;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.fechaNacimiento = fechaNacimiento;
-		this.sexo = sexo;
-	}
-	
-	public Persona(String datos) {
-		String[] atributo = datos.split("\t");
-		this.idPersona=Integer.parseInt(atributo[0]);
-		this.dni=Integer.parseInt(atributo[1]);
-		this.tipoDoc=Integer.parseInt(atributo[2]);
-		this.nombre=atributo[3];
-		this.apellido=atributo[4];
-		this.fechaNacimiento=LocalDate.parse(atributo[5]);
-		this.sexo=atributo[6];
-		
-	}
-
 	public Persona() {
 		super();
 	}
 
+	public Persona(Integer idPersona, Integer dni, Integer tipoDoc, String nombre, String apellido, LocalDate fechaNacimiento, String sexo) {
+		super();
+		this.dni			 = dni;
+		this.tipoDoc 		 = tipoDoc;
+		this.nombre 		 = nombre;
+		this.apellido 		 = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.sexo 			 = sexo;
+	}
+	
 	public Integer getId() {
 		return idPersona;
 	}
